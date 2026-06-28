be2ff11134316462599759e68781b34c9c88189d
raushank7460 <raushank7460@gmail.com>
2026-06-28 12:51:12 +0530

    add

--- backTracking/GFGratinmaze.java
+++ backTracking/GFGratinmaze.java
@@ -7,9 +7,31 @@ public class GFGratinmaze {
                 {1, 1, 0, 1}, 
                 {1, 1, 0, 0}, 
                 {0, 1, 1, 1}
-         };
+         }; 
+        int rows=maze.length;
+        int cols=maze[0].length;
+        boolean[][]isVisited=new boolean[rows][cols];
+        print(0,0,rows-1,cols-1,"",isVisited,maze);
 
         
+    }
+    public static void print(int sr,int sc,int er,int ec,String s,boolean[][]isVisited,int[][]maze){
+        if(sr<0 || sc<0 || sr>er || sc>ec) return;
+        if(maze[sr][sc]==0)return;
+        if(sr==er && sc==ec){
+            System.out.println(s);
+            return;
+        }
+        if(isVisited[sr][sc])return;
+        isVisited[sr][sc]=true;
+        print(sr, sc+1, er, ec, s+"R", isVisited, maze);
+        print(sr+1, sc, er, ec, s+"D", isVisited, maze);
+        print(sr, sc-1, er, ec, s+"L", isVisited, maze);
+        print(sr-1, sc, er, ec, s+"U", isVisited, maze);
+        isVisited[sr][sc]=false;
+
+        
+
     }
     
 }
